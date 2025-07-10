e9ec924a610ed29cae67bd01896bb74761208d98
Rohitpawar0005 <chrohit84@gmail.com>
2025-07-11 01:15:52 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -8,14 +8,28 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-app.use("/api/users", userRouter);
+const dbuser = encodeURIComponent(process.env.DBUSER);
+const dbpass = encodeURIComponent(process.env.DBPASS);
 
-mongoose.connect(`mongodb://localhost:27017/merncafe`)
+// mongoose.connect(`mongodb://localhost:27017/merncafe`)
+//   .then(() => {
+//     app.listen(8080, () => {
+//       console.log("✅ Server started on http://localhost:8080");
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("❌ MongoDB connection error:", err);
+//   });
+
+  mongoose
+  .connect(
+    `mongodb+srv://${dbuser}:${dbpass}@cluster0.qjxhv.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+  )
   .then(() => {
     app.listen(8080, () => {
-      console.log("✅ Server started on http://localhost:8080");
+      console.log("Server started");
     });
-  })
-  .catch((err) => {
-    console.error("❌ MongoDB connection error:", err);
   });
+
+
+app.use("/api/users", userRouter);
