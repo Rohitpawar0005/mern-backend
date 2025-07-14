fbe8f0f08a995ccb053ebcd22922207fca027ede
Rohitpawar0005 <chrohit84@gmail.com>
2025-07-14 17:14:00 +0530

    modified:   routes/userRoute.js 	modified:   server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
-import cors from 'cors'
+// import productRouter from "./routes/productRoute.js";
+import cors from "cors";
 
 
+dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(cors())
+
 
 const dbuser = encodeURIComponent(process.env.DBUSER);
 const dbpass = encodeURIComponent(process.env.DBPASS);
@@ -21,15 +25,16 @@ const dbpass = encodeURIComponent(process.env.DBPASS);
 //     console.error("❌ MongoDB connection error:", err);
 //   });
 
-  mongoose
-  .connect(
-    `mongodb+srv://${dbuser}:${dbpass}@cluster0.qjxhv.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
-  )
-  .then(() => {
-    app.listen(8080, () => {
-      console.log("Server started");
-    });
-  });
+   mongoose
+   .connect(
+     `mongodb+srv://${dbuser}:${dbpass}@cluster0.jkws10l.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+   )
+   .then(() => {
+     app.listen(8080, () => {
+       console.log("Server started");
+   });
+   });
 
 
 app.use("/api/users", userRouter);
+
